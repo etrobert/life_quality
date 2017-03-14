d41fb5e554b9a201f47a549e3de6a73a0bf17e37
etrobert <etrobert@debian.totoro>
2017-03-14 15:38:06 +0100

    Updates

--- calculate.js
+++ calculate.js
@@ -1,4 +1,4 @@
-function update_result(values, result)
+function update_result(values, result, result_div)
 {
 	var n;
 
@@ -6,14 +6,15 @@ function update_result(values, result)
 	for (var value of values)
 	{
 		if (value.max == "7")
-			n += (parseInt(value.value) - 1) * 3;
+			n += (value.value - 1) * 3;
 		else if (value.max == "4")
-			n += (parseInt(value.value) - 1) * 6;
+			n += (value.value - 1) * 6;
 		else if (value.max == "2")
-			n += (parseInt(value.value) - 1) * 18;
+			n += (value.value - 1) * 18;
 		else
 			console.log("Wrong max");
 	}
+	result_div.hidden = false;
 	result.innerHTML = Math.floor(n * 100 / (values.length * 18));
 }
 
@@ -27,11 +28,12 @@ function main()
 	var	submit = document.getElementById("submit");
 	var values = document.querySelectorAll(".value");
 	var result = document.getElementById("result");
+	var	result_div = document.getElementById("result_div");
 
 	//submit.addEventListener("click", update_result.bind(null, event, values, result));
 	submit.addEventListener("click", function (event) {
 		event.preventDefault();
-		update_result(values, result); });
+		update_result(values, result, result_div); });
 }
 
 main();
